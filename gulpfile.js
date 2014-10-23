fa5782a882492073a2b5dc26537565ff1f279134
Jeremy Wilken <gnomation@gnomeontherun.com>
2014-10-22 23:00:06 -0500

    use sass, move location

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,13 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./www/scss/**/*.scss']
 };
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+  gulp.src('./www/scss/ionic.app.scss')
     .pipe(sass())
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
